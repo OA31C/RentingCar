df989470941e020afa337b38b4f56bd556de1812
OA3IC <OA3IC.QV@gmail.com>
2020-09-25 15:14:04 +0300

    Create User

--- apps/auth_user/models.py
+++ apps/auth_user/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class User(AbstractUser):
+    languages = (
+        (1, 'en'),
+        (2, 'ru')
+    )
+    email = models.EmailField(max_length=60, unique=True)
+    language = models.IntegerField(
+        verbose_name='language', choices=languages, default='1')
+
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return f"id:{self.id}, {self.username}"

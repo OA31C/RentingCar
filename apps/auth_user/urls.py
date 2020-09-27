e36f7b53fe2e32e4db5c0f47499d5fc4b03a51a5
OA3IC <OA3IC.QV@gmail.com>
2020-09-27 12:07:14 +0300

    Create user profile, user edit and user delete function and login requirmend to see pages except login and register pages

--- apps/auth_user/urls.py
+++ apps/auth_user/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path, include
-from .views import RegistrationUser, LoginUser, LogOutUser
-from renting_car.views import redirect_login
+from .views import RegistrationUser, LoginUser, LogOutUser, UserProfile, UserEdit, UserDelete
 from django.contrib.auth import views as auth_views
+from renting_car.views import redirect_login
+from django.urls import path, include
 
 
 urlpatterns = [
@@ -10,15 +10,23 @@ urlpatterns = [
     path('login/', LoginUser.as_view(), name='login_url'),
     path('logout/', LogOutUser.as_view(), name='logout_url'),
 
+    path('user/profile/<int:pk>/', UserProfile.as_view(), name='user_profile_url'),
+    path('user/edit/<int:pk>/', UserEdit.as_view(), name='user_edit_url'),
+    path('user/delete/<int:pk>/', UserDelete.as_view(), name='user_delete_url'),
+
     path('reset_password/', auth_views.PasswordResetView.as_view(
-        template_name='auth_user/password_reset/password_reset.html'), name='reset_password'),
+        template_name='auth_user/password_reset/password_reset.html'),
+        name='reset_password'),
+
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(
-        template_name='auth_user/password_reset/password_reset_sent.html'
-    ), name='password_reset_done'),
+        template_name='auth_user/password_reset/password_reset_sent.html'),
+        name='password_reset_done'),
+
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='auth_user/password_reset/password_reset_form.html'
-    ), name='password_reset_confirm'),
+        template_name='auth_user/password_reset/password_reset_form.html'),
+        name='password_reset_confirm'),
+
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='auth_user/password_reset/password_reset_done.html'
-    ), name='password_reset_complete'),
+        template_name='auth_user/password_reset/password_reset_done.html'),
+        name='password_reset_complete'),
 ]
--- apps/auth_user/views.py
+++ apps/auth_user/views.py
@@ -1,26 +1,72 @@
-from django.shortcuts import render
-from django.views.generic import View
-from django.contrib.auth import views as auth_views
-from django.views import generic
-from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
 from .forms import LoginForm, RegisterForm
+from django.views.generic import View
+from django.http import HttpResponse
+from .models import User
 
 
-class RegistrationUser(generic.CreateView):
+class RegistrationUser(CreateView):
     form_class = RegisterForm
     template_name = 'auth_user/registration.html'
     success_url = reverse_lazy('login_url')
 
 
-class LoginUser(auth_views.LoginView):
+class LoginUser(LoginView):
     form_class = LoginForm
     template_name = 'auth_user/login.html'
 
-    """Override method to the url"""
+    """Override method to go to the login"""
     def get_success_url(self):
-        return reverse_lazy('login_url')
+        auth_user = self.request.user
+        return reverse('user_profile_url',  kwargs={'pk': auth_user.pk})
 
 
-class LogOutUser(auth_views.LogoutView):
+class LogOutUser(LogoutView):
     next_page = reverse_lazy('login_url')
 
+
+class UserProfile(LoginRequiredMixin, View):
+    login_url = 'login_url'
+
+    def get(self, request, pk):
+        user = User.objects.get(id=pk)
+        return render(request, 'auth_user/user_profile.html', context={'user': user})
+
+
+class UserEdit(LoginRequiredMixin, UpdateView):
+    login_url = 'login_url'
+    model = User
+    fields = ['username', 'email', 'language']
+    template_name = 'auth_user/user_edit.html'
+
+    """Override method to go to the user profile"""
+    def get_success_url(self):
+        object_user = self.request.user
+        return reverse('user_profile_url', kwargs={'pk': object_user.pk})
+
+    def get_form_kwargs(self):
+        """Return the keyword arguments for instantiating the form."""
+        kwargs = super().get_form_kwargs()
+        if self.request.user.email != kwargs['instance'].email:
+            return self.handle_no_permission()
+        return kwargs
+
+
+class UserDelete(LoginRequiredMixin, View):
+    login_url = 'login_url'
+
+    def get(self, request, pk):
+        user = User.objects.get(id=pk)
+        return render(request, 'auth_user/user_delete.html', context={'user': user})
+
+    def post(self, request, pk):
+        user = User.objects.get(id=pk)
+        logged_in_user = self.request.user
+        if logged_in_user.email != user.email:
+            return HttpResponse('<h1>This user has no rights to this action</h1>')
+        user.delete()
+        return redirect('login_url')

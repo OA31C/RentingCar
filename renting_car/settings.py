067d3a66070f107dbb63749633234198cc807957
OA3IC <OA3IC.QV@gmail.com>
2020-10-08 18:30:47 +0300

    Add setings to connection to PostgresSQL DB

--- renting_car/settings.py
+++ renting_car/settings.py
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'renting_car.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'RantingCarDB',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 

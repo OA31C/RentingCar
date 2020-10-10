d52ee14511c84a2b3a146acd25ab60a422e6d3d6
OA3IC <OA3IC.QV@gmail.com>
2020-10-10 19:54:21 +0300

    Fixed to create renting car

--- apps/car/views.py
+++ apps/car/views.py
@@ -1,14 +1,13 @@
 from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.translation import ugettext as _
-from renting_car.settings import EMAIL_HOST_USER
 from apps.car.models import Car, RentingCar
-from django.core.mail import send_mail
 from django.views.generic import View
 from django.shortcuts import redirect
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib import messages
+from .helpers import exception_email
 
 
 # Create your views here.
@@ -25,8 +24,7 @@ class CarCreate(LoginRequiredMixin, CreateView):
         if entered_data_form.save():
             messages.add_message(self.request, messages.SUCCESS, _('Car is create success!'))
             message = f"Car is create - {entered_data_form.name_en}"
-            send_mail('Renting Car Django', message,
-                      EMAIL_HOST_USER, [self.request.user.email])
+            exception_email(message=message, send_to=[self.request.user.email])
         else:
             messages.add_message(self.request, messages.SUCCESS, _('Incorrect data entered!'))
         return super().form_valid(form)
@@ -96,14 +94,16 @@ class CarRentingAdd(LoginRequiredMixin, CreateView):
     template_name = 'car_renting/car_renting_add.html'
 
     def form_valid(self, form):
-        self.object = form.save(commit=False)
-        self.object.user = self.request.user
-        self.object.save()
-        message = f"Book car {self.object.car.name_en} is create." \
-                  f"Forget date {self.object.lease_start_date} - {self.object.lease_end_date}"
-        send_mail('Renting Car Django', message,
-                  EMAIL_HOST_USER, [self.request.user.email])
-        messages.add_message(self.request, messages.SUCCESS, _('Renting car added is success!'))
+        entered_data_form = form.save(commit=False)
+        entered_data_form.user = self.request.user
+
+        if entered_data_form.save():
+            message = f"Book car {entered_data_form.car.name_en} is create." \
+                      f"Forget date {entered_data_form.lease_start_date} - {entered_data_form.lease_end_date}"
+            messages.add_message(self.request, messages.SUCCESS, _('Renting car added is success!'))
+            exception_email(message=message, send_to=[self.request.user.email])
+        else:
+            messages.add_message(self.request, messages.SUCCESS, _('Renting car added not success!'))
         return super().form_valid(form)
 
     def get_success_url(self):
